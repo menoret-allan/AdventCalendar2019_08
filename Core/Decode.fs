f9e296d4a6760f6f5bab7423d1670321604304dc
Allan Menoret <allan.menoret@gmail.com>
2019-12-16 16:27:21 +0100

    part 1 done

---
+++ Core/Decode.fs
@@ -0,0 +1,21 @@
+﻿namespace Core
+
+open System
+
+module Decode =
+
+    type Dimension = (int * int)
+    type Layer = string list
+    type Image = {dimension:Dimension; layers:Layer list}
+
+    let parse (input: string) width height =
+        let digits = input |> Seq.toList
+        let layers =
+            digits
+            |> List.chunkBySize (width * height)
+            |> List.map (fun layer -> layer |> List.chunkBySize width |> List.map (fun pixel -> pixel |> String.Concat))
+        {dimension=(width, height); layers=layers}
+
+
+
+
--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -2,7 +2,25 @@ module Tests
 
 open System
 open Xunit
+open FsUnit
+open Core.Decode
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Parse into 2 layers`` () =
+    let input = "123456789012"
+    let image = parse input 3 2
+    image.layers |> should haveLength 2
+
+[<Fact>]
+let ``Parse generate width with 3 of size`` () =
+    let input = "123456789012"
+    let image = parse input 3 2
+    image.layers |> List.head |> should haveLength 2
+    image.layers |> List.last |> should haveLength 2
+
+[<Fact>]
+let ``Parse generate with correct values`` () =
+    let input = "123456789012"
+    let image = parse input 3 2
+    image.layers |> List.concat |> should equivalent [123;456;789;012] 
+
